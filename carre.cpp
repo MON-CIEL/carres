0c0e2afb45adfe438d9d45019b62b43472bc5001
MON-CIEL <mamonteirogarcia@gmail.com>
2024-12-16 20:25:07 +0100

    Commit #7 "Creation d'un tableau de 4 carrées et amelioration de l'affichage"

--- carre.cpp
+++ carre.cpp
@@ -1,4 +1,5 @@
 #include <iostream> /* Lib Cin , Cout */
+#include <iomanip> /* Lib Set... , hex , dec , ...*/
 #include "carre.h"
 using namespace std;
 
@@ -22,15 +23,41 @@ void CCarre::Setcote(int cote1) {
 }
 
 void CCarre::Afficher() {
-	cout << endl << "Le carre a pour coordonnees sx la valeur suivante " << this->sx << " et la valeur retournee est de " << Getsx() << endl;
-	cout << endl << "Le carre a pour coordonnees sy la valeur suivante " << this->sy << " et la valeur retournee est de " << Getsy() << endl;
-	cout << endl << "Le carre a pour cote la valeur suivante " << this->cote << " et la valeur retournee est de " << GetCote() << endl;
+	//cout << endl << "Le carre a pour coordonnees sx la valeur suivante " << this->sx << " et la valeur retournee est de " << Getsx() << endl;
+	//cout << endl << "Le carre a pour coordonnees sy la valeur suivante " << this->sy << " et la valeur retournee est de " << Getsy() << endl;
+	//cout << endl << "Le carre a pour cote la valeur suivante " << this->cote << " et la valeur retournee est de " << GetCote() << endl;
+	
+	cout << endl << "Le carre a pour valeurs , les valeurs suivantes :  " << endl;
+
+	/* Tableau */
+	cout << endl << setw(24) << "" << "-------------------------------------------------------" << endl;
+	cout << setw(25) << "|" << setw(27) << "Valeur |" << setw(27) << "Valeur retournee |" << endl;
+	cout << "-------------------------------------------------------------------------------" << endl;
+	cout << "|" << setw(24) << "Position Sx |" << setw(25) << sx << " |" << setw(25) << Getsx() << " |" << endl;
+	cout << "-------------------------------------------------------------------------------" << endl;
+	cout << "|" << setw(24) << "Position Sy |" << setw(25) << sy << " |" << setw(25) << Getsy() << " |" << endl;
+	cout << "-------------------------------------------------------------------------------" << endl;
+	cout << "|" << setw(24) << "Valeur Cote |" << setw(25) << cote << " |" << setw(25) << GetCote() << " |" << endl;
+	cout << "-------------------------------------------------------------------------------" << endl;
+
 }
 
 void CCarre::AfficherDeplacement() {
-	cout << endl << "Le carre a pour coordonnees sx apres deplacement la valeur suivante " << this->sx << " et la valeur retournee est de " << Getsx() << endl;
-	cout << endl << "Le carre a pour coordonnees sy apres deplacement la valeur suivante " << this->sy << " et la valeur retournee est de " << Getsy() << endl;
-	cout << endl << "Le carre a pour cote la valeur suivante " << this->cote << " et la valeur retournee est de " << GetCote() << endl;
+	//cout << endl << "Le carre a pour coordonnees sx apres deplacement la valeur suivante " << this->sx << " et la valeur retournee est de " << Getsx() << endl;
+	//cout << endl << "Le carre a pour coordonnees sy apres deplacement la valeur suivante " << this->sy << " et la valeur retournee est de " << Getsy() << endl;
+	//cout << endl << "Le carre a pour cote la valeur suivante " << this->cote << " et la valeur retournee est de " << GetCote() << endl;
+
+	cout << endl << "Le carre a pour valeurs apres deplacement , les valeurs suivantes :  " << endl;
+	/* Tableau */
+	cout << endl << setw(24) << "" << "-----------------------------------------------------------------------" << endl;
+	cout << setw(25) << "|" << setw(35) << "Valeur ->Deplacement |" << setw(35) << "Valeur retournee ->Deplacement |" << endl;
+	cout << "-----------------------------------------------------------------------------------------------" << endl;
+	cout << "|" << setw(24) << "Position Sx |" << setw(33) << sx << " |" << setw(33) << Getsx() << " |" << endl;
+	cout << "-----------------------------------------------------------------------------------------------" << endl;
+	cout << "|" << setw(24) << "Position Sy |" << setw(33) << sy << " |" << setw(33) << Getsy() << " |" << endl;
+	cout << "-----------------------------------------------------------------------------------------------" << endl;
+	cout << "|" << setw(24) << "Valeur Cote |" << setw(33) << cote << " |" << setw(33) << GetCote() << " |" << endl;
+	cout << "-----------------------------------------------------------------------------------------------" << endl;
 }
 
 
--- mcarre.cpp
+++ mcarre.cpp
@@ -16,17 +16,81 @@ using namespace std;
 
 
 int main() {
+	
+	/* Declarations Variables */
 	CCarre a;
-	a.Setsx(2);
-	a.Setsy(3);
-	a.Setcote(4);
+	int valeurSx;
+	int valeurSy;
+	int valeurCote;
+	int direction;
+
+
+	// Test Methode Get , Set et Afficher
+	cout << endl << "Veuillez saisir une valeur pour la position sx du carree" << endl;
+	cout << endl << "Valeur pour sx : ";
+	cin >> valeurSx;
+	a.Setsx(valeurSx);
+	cout << endl << "Veuillez saisir une valeur pour la position sy du carree" << endl;
+	cout << endl << "Valeur pour sy : ";
+	cin >> valeurSy;
+	a.Setsy(valeurSy);
+	cout << endl << "Veuillez saisir une valeur pour le cote du carree" << endl;
+	cout << endl << "Valeur de cote : ";
+	cin >> valeurCote;
+	a.Setcote(valeurCote);
 	a.Afficher();
-	a.Deplacer('o', 6);
-	a.Deplacer('n', 4);
+	
+
+	
+	//Tets Methode Deplacer
+	cout << endl << "Veuillez saisir le nombre de direction pour les lequelles vous voulez deplacer le carrée" << endl;
+	cout << endl << "Nombre de directions : ";
+	cin >> direction;
+	for (int i = 1; i <= direction; i++){
+		int deplacement;
+		char dDirection;
+		cout << endl << "Veuillez saisir vers ou vous comptez deplacer votre carree en ecrivant ( n pour Nord , s pour Sud , o pour Ouest et e pour Est) " << endl;
+		cout << endl << "Direction : ";
+		cin >> dDirection;
+		cout << endl << "Veuillez saisir de combien vous comptez deplacer le carree dans la direction choisie" << endl;
+		cout << endl << "Pas de deplacement : ";
+		cin >> deplacement;
+		a.Deplacer(dDirection, deplacement);
+		a.AfficherDeplacement();
+	}
+
+
+	// Test de Surchage de la methode Deplacer
+	int deplacementx;
+	int deplacementy;
+	cout << endl << "Veuillez saisir de combien vous comptez deplacer le carree a la position sx " << endl;
+	cout << endl << "Pas de deplacement : ";
+	cin >> deplacementx;
+	cout << endl << "Veuillez saisir de combien vous comptez deplacer le carree a la position sy" << endl;
+	cout << endl << "Pas de deplacement : ";
+	cin >> deplacementy;
+	a.Deplacer(deplacementx , deplacementy);
 	a.AfficherDeplacement();
-	a.Deplacer('e', 9);
-	a.Deplacer('s', 12);
-	a.AfficherDeplacement();
-	a.Deplacer(10, 10);
-	a.AfficherDeplacement();
+
+
+	// Creation Tableau de 4 carrée 
+	int const taille = 4;
+	CCarre carre[taille];
+
+	for (size_t i = 0; i < taille; i++){
+		cout << endl << "Veuillez saisir une valeur pour la position sx du carree" << endl;
+		cout << endl << "Valeur pour sx : ";
+		cin >> valeurSx;
+		carre[i].Setsx(valeurSx);
+		cout << endl << "Veuillez saisir une valeur pour la position sy du carree" << endl;
+		cout << endl << "Valeur pour sy : ";
+		cin >> valeurSy;
+		carre[i].Setsy(valeurSy);
+		cout << endl << "Veuillez saisir une valeur pour le cote du carree" << endl;
+		cout << endl << "Valeur de cote : ";
+		cin >> valeurCote;
+		carre[i].Setcote(valeurCote);
+		cout << endl << "Voici les valeurs pour le carree " << i+1 << endl;
+		carre[i].Afficher();
+	}
 }
